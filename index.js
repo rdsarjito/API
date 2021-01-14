cc00c2ecedde727e406a217e060565d28fc72f77
Ramadhani Nursarjito <rama.dhaninursarjito@gmail.com>
2021-01-14 16:06:57 +0700

    express static

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
 const app = express();
 
 app.get('/api', (req, res) => {
   const obj = {
-    name: 'rama'
+    name: 'rama',
+    test: 'test'
   }
   res.send(obj);
 });
 
-console.log("test")
+
+app.use('/images', express.static('images'));
+
+console.log("test");
 app.listen(5000);
